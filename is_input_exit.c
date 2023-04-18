27df4b17b2c575e98640a11d0b4705554a146aeb
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-04-18 08:47:17 -0700

    input equal exit

--- is_input_exit.c
+++ is_input_exit.c
@@ -1,12 +1,19 @@
 #include "simple_shell.h"
 
+/**
+ * is_input_exit - check if input equal to exit.
+ * @args: users's input.
+ *
+ * Return: 1 on success. 0 otherwise.
+ */
+
 int is_input_exit(char **args)
 {
 	int i = 0, len = 0;
 
-	while(args[i++])
+	while (args[i++])
 		len++;
-	
+
 	if (strcmp(args[0], "exit") == 0 && len == 1)
 		return (1);
 	return (0);
