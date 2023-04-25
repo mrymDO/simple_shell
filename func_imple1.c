f9af0138b012cfd39082be54df37aeda5ee7d315
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-04-25 08:43:33 -0700

    fix

--- func_imple1.c
+++ func_imple1.c
@@ -108,10 +108,9 @@ char *_strchr(char *s, char c)
 		s++;
 	}
 	if (*s != c)
-		return ('\0');
+		return ("\0");
 
 	return (s);
-
 }
 
 
--- split_dirs.c
+++ split_dirs.c
@@ -9,17 +9,10 @@
 
 char **split_dirs(char *env_dirs)
 {
-<<<<<<< HEAD
 	char **arr_dirs = NULL;
 	int i = 0, num_dirs = 0;
 	char *piece;
 	char *env_dirs_copy;
-=======
-        char **arr_dirs = NULL;
-        int i = 0, num_dirs = 0, j;
-        char *piece;
-        char *env_dirs_copy = NULL;
->>>>>>> a4e13f7ba565407c56907b28971766d4fafbce66
 
 	if (env_dirs)
 		env_dirs_copy = _strdup(env_dirs);
@@ -41,14 +34,6 @@ char **split_dirs(char *env_dirs)
 	{
 		arr_dirs[i] = malloc(sizeof(char) * (_strlen(piece) + 1));
 		if (arr_dirs[i] == NULL)
-<<<<<<< HEAD
-=======
-		{
-			for (j = 0; j < i; j++)
-                		free(arr_dirs[j]);
-			free(arr_dirs);
-			free(env_dirs_copy);
->>>>>>> a4e13f7ba565407c56907b28971766d4fafbce66
 			return (NULL);
 		_strcpy(arr_dirs[i], piece);
 		piece = strtok(NULL, ":");
